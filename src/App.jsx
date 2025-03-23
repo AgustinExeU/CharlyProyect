80bead25fc41a5049464c42d42a803d0b32c0946
Charly <charlycarranza71@gmail.com>
2025-03-23 11:01:47 -0600

    Corregir rutas de importación

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import { Routes, Route } from "react-router-dom";
-import LoginPage from "./pages/LoginPage";
-import RegisterPage from "./pages/RegisterPage";
-import Home from "./pages/Home";
-import ProductoDetalle from "./pages/ProductoDetalle";
-import Contactos from "./pages/Contactos";
-import Perfil from "./pages/Perfil";
-import MainLayout from "./layouts/MainLayout";
-import Dashboard from "./pages/Dashboard";
-import ErrorPage from "./pages/ErrorPage";
-import ForgotPasswordPage from "./pages/ForgotPasswordPage";
-import ProtectedRoute from "./components/ProtectedRoute"; 
+import LoginPage from "./pages/LoginPage.jsx"; 
+import RegisterPage from "./pages/RegisterPage.jsx";
+import Home from "./pages/Home.jsx";
+import ProductoDetalle from "./pages/ProductoDetalle.jsx";
+import Contactos from "./pages/Contactos.jsx";
+import Perfil from "./pages/Perfil.jsx";
+import MainLayout from "./layouts/MainLayout.jsx";
+import Dashboard from "./pages/Dashboard.jsx";
+import ErrorPage from "./pages/ErrorPage.jsx";
+import ForgotPasswordPage from "./pages/ForgotPasswordPage.jsx";
+import ProtectedRoute from "./components/ProtectedRoute.jsx";
 
 function App() {
   return (
